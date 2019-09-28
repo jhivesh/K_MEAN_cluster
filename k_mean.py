899a5db88d900564a352b0e023d8f2aaad361672
jhivesh <46216085+jhivesh@users.noreply.github.com>
2019-09-28 19:23:49 +0400

    full kmeans cluster

--- k_mean.py
+++ k_mean.py
@@ -1,24 +1,113 @@
-#%matplotlib inline
-import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
+import math  # For pow and sqrt
+import sys
+from random import shuffle, uniform
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+###_Pre-Processing_###
 from sklearn.cluster import KMeans
 
-from sklearn.datasets.samples_generator import make_blobs
-X, y_true = make_blobs(n_samples = 400, centers = 4, cluster_std = 0.60, random_state = 0)
 
-plt.scatter(X[:, 0], X[:, 1], s = 20);
-plt.show()
+def ReadData(fileName):
+    f = open("fileName", "r")
+    lines = f.read().splitlines()
+    f.close()
+
+    item = []
+
+    for i in range(1, len(lines)):
+        line = lines[i].split(',')
+        itemFeatures = []
+
+        for j in range(len(line) - 1):
+            v = float(line[j])  # Convert feature value to float
+            itemFeatures.append(v)  # Add feature value to dict
+
+        items.append(itemFeatures)
+
+    shuffle(items)
+
+    return items
+
+###_Auxiliary Function_###
+def FindColMinMax(items):
+    n = len(items[0])
+    minima = [sys.maxsize for i in range(n)]
+    maxima = [-sys.maxsize - 1 for i in range(n)]
+
+    for item in items:
+        for f in range(len(item)):
+            if (item[f] < minima[f]):
+                minima[f] = item[f]
+
+            if (item[f] > maxima[f]):
+                maxima[f] = item[f]
+
+    return minima, maxima
 
 
-kmeans = KMeans(n_clusters = 4)
-kmeans.fit(X)
-y_kmeans = kmeans.predict(X)
+def EuclideanDistance(x, y):
+    S = 0  # The sum of the squared differences of the elements
+    for i in range(len(x)):
+        S += math.pow(x[i] - y[i], 2)
 
-from sklearn.datasets.samples_generator import make_blobs
-X, y_true = make_blobs(n_samples = 400, centers = 4, cluster_std = 0.60, random_state = 0)
+    return math.sqrt(S)  # The square root of the sum
 
-plt.scatter(X[:, 0], X[:, 1], c = y_kmeans, s = 20, cmap = 'summer')
-centers = kmeans.cluster_centers_
-plt.scatter(centers[:, 0], centers[:, 1], c = 'blue', s = 100, alpha = 0.9);
-plt.show()
+
+
+def InitializeMean(items, k, cMin, cMax):
+    # Initialize means to random numbers between
+    # the min and max of each column/feature
+
+    f = len(items[0])     # the number of features
+    means = [ [ 0 for i in range(f) for j in range(k)]]
+
+    for mean in means:
+        for i in range(len(mean)):
+            # Set value to a random float
+            # (adding +-1 to avoid a wide placement of a mean)
+            mean[i] = uniform(cMin[i] + 1, cMax[i] - 1)
+
+    return means
+
+
+def UpdateMean(n, mean, item):
+    for i in range(len(mean)):
+        m = mean[i]
+        m = (m * (n - 1) + item[i]) / float(n)
+        mean[i] = round(m, 3)
+
+    return mean
+
+
+def FindClusters(means, items):
+    clusters = [[] for i in range(len(means))]  # Init clusters
+
+    for item in items:
+        # Classify item into a cluster
+        index = Classify(means, item)
+
+        # Add item to cluster
+        clusters[index].append(item)
+
+    return clusters
+
+
+
+###_Core Functions_###
+def Classify(means, item):
+    # Classify item to the mean with minimum distance
+
+    minimum = sys.maxsize
+    index = -1
+
+    for i in range(len(means)):
+        # Find distance from item to mean
+        dis = EuclideanDistance(item, means[i])
+
+        if (dis < minimum):
+            minimum = dis
+            index = i
+
+    return index
